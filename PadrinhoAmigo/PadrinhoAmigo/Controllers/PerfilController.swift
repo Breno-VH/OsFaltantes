246a47915babce9fd3ca90d9b42d169907e2bfca
lariokabayashi <l247220@dac.unicamp.br>
2023-06-30 09:06:52 -0300

    Creating storyboards and files

---
+++ PadrinhoAmigo/PadrinhoAmigo/Controllers/PerfilController.swift
@@ -0,0 +1,18 @@
+//
+//  PerfilController.swift
+//  PadrinhoAmigo
+//
+//  Created by Larissa Okabayashi on 30/06/23.
+//
+
+import UIKit
+
+class PerfilController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}

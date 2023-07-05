ae517915cc095800ad029edef2bbe7e086b8d48e
Breno <brenovitral@hotmail.com>
2023-07-05 11:40:47 -0300

    copiando a lari

--- PadrinhoAmigo/PadrinhoAmigo/Models/UserCell.swift
+++ PadrinhoAmigo/PadrinhoAmigo/Models/UserCell.swift
@@ -13,18 +13,43 @@ class UserCell: UITableViewCell {
     
     @IBOutlet weak var img: UIImageView!
     
-  
+    @IBOutlet weak var course: UILabel!
+    
+    @IBOutlet weak var year: UILabel!
+    
+    @IBOutlet weak var interests: UILabel!
+    
     func show(user: User){
-        
-        
+        //name.text = user.getName(n: i)
         name.text = user.name
         img.image = user.img
-        
         img.layer.borderWidth = 1
         img.layer.masksToBounds = false
         img.layer.borderColor = UIColor.black.cgColor
         img.layer.cornerRadius = img.frame.height/2
         img.clipsToBounds = true
-        
+        course.text = user.course
+        year.text = "0" + String(user.year!)
+        interests.text = format(interests: user.interests!)
+      }
+      func format(interests: [String]) -> String{
+        var interestsString: String = ""
+        for i in interests{
+          interestsString += i
+          if (i != interests[interests.count-1]){
+            interestsString += " - "
+          }
+        }
+        return interestsString
+      }
     }
-}
+
+
+
+
+
+
+
+
+
+

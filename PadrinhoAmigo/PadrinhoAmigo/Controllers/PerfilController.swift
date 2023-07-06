01e77e23d6a36769a02f1ba5bd7727f22f6fab3f
lariokabayashi <l247220@dac.unicamp.br>
2023-07-06 09:15:32 -0300

    updating main

--- PadrinhoAmigo/PadrinhoAmigo/Controllers/PerfilController.swift
+++ PadrinhoAmigo/PadrinhoAmigo/Controllers/PerfilController.swift
@@ -6,13 +6,16 @@
 //
 
 import UIKit
-
 class PerfilController: UIViewController {
-
+   
+    @IBOutlet weak var ForegroundView: UIView!
+    @IBOutlet weak var BackgroundView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-
-
+        BackgroundView.clipsToBounds = true
+        ForegroundView.clipsToBounds = true
+        ForegroundView.layer.cornerRadius = 30
+        ForegroundView.layer.maskedCorners = [.layerMaxXMinYCorner, .layerMinXMinYCorner]
+      }
 }

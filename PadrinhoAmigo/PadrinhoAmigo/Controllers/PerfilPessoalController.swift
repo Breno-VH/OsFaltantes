f5dbae3989ab4f6b97c98be669a447a4187703f1
lariokabayashi <l247220@dac.unicamp.br>
2023-07-11 17:45:33 -0300

    updating perfil

--- PadrinhoAmigo/PadrinhoAmigo/Controllers/PerfilPessoalController.swift
+++ PadrinhoAmigo/PadrinhoAmigo/Controllers/PerfilPessoalController.swift
@@ -10,9 +10,52 @@ import UIKit
 
 class PerfilPessoalController: UIViewController {
     
+    let categories = [
+    "Curso",
+    "Ano que entrou",
+    "Origem",
+    "Entidades",
+    "Interesses",
+    "Experiência",
+    "Descrição"
+    ]
+    
+    @IBOutlet weak var perfilTableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        perfilTableview.delegate = self
+        perfilTableview.dataSource = self
         
       }
+    
 }
+
+extension PerfilPessoalController: UITableViewDataSource, UITableViewDelegate{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        categories.count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 6{
+           return 100
+        }
+        return 74
+    }
+    
+    func tableView(_ tableView: UITableView, widthForRowAt indexPath: IndexPath) -> CGFloat {
+        361
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: PerfilCell = perfilTableview.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! PerfilCell
+        cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        cell.textLabel?.text = categories[indexPath.row]
+        //infoLabel.numberOfLines = 3;
+        //infoLabel.lineBreakMode = .byWordWrapping
+        //cell.infoPersons(user: displayUser, i: indexPath.row)
+        return cell
+    }
+    
+}
+

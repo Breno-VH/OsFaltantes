bac1fafb993d29cd0b462a89b539096000e7f8c2
lariokabayashi <l247220@dac.unicamp.br>
2023-07-11 11:31:50 -0300

    modifications

---
+++ PadrinhoAmigo/PadrinhoAmigo/Models/LoginCell.swift
@@ -0,0 +1,14 @@
+//
+//  LoginCell.swift
+//  PadrinhoAmigo
+//
+//  Created by Larissa Okabayashi on 11/07/23.
+//
+
+import Foundation
+import UIKit
+
+class LoginCell: UITableViewCell {
+    
+    
+}

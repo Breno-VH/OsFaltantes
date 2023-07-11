f3104fdf4ec9cbea7ee686f53b87bf0a6571b8eb
lariokabayashi <l247220@dac.unicamp.br>
2023-07-11 17:40:31 -0300

    creating perfil

---
+++ PadrinhoAmigo/PadrinhoAmigo/Controllers/PerfilPessoalController.swift
@@ -0,0 +1,18 @@
+//
+//  PerfilPessoal.swift
+//  PadrinhoAmigo
+//
+//  Created by Larissa Okabayashi on 11/07/23.
+//
+
+import Foundation
+import UIKit
+
+class PerfilPessoalController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+      }
+}

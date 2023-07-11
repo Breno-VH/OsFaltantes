d4aed243b22c679f953cfb1bc8e91452c15046cb
Breno <brenovitral@hotmail.com>
2023-07-11 09:55:38 -0300

    adicionando registrar perfil ocntroller

--- PadrinhoAmigo/PadrinhoAmigo/Controllers/LoginController.swift
+++ PadrinhoAmigo/PadrinhoAmigo/Controllers/LoginController.swift
@@ -18,31 +18,6 @@ class LoginController: UIViewController {
         
       }
     
-    func saveUser(user: User) async {
-        manager.save(user: user)
-        
-    }
-
-}
-
-
-extension LoginController {
     
-    func validateEmail(email: String) -> Bool {
-        do {
-            let pattern = "[a-z]{1}[0-9]{6}@dac.unicamp.br"
-            let academicEmailRegex = try NSRegularExpression(pattern: pattern)
-            let range = NSRange(location: 0, length: email.count)
-            
-            if academicEmailRegex.firstMatch(in: email, range: range) != nil {
-                return true
-            }
-            
-        } catch let error {
-            print(error.localizedDescription)
-            return false
-        }
-        return false
-    }
-
 }
+
